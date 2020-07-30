255e49b7441429ea6fa386fdaa9d8576e99a526b
cbot95 <chloe.talbot95@gmail.com>
2020-07-30 10:56:16 +0100

    add other toastst

--- src/app/game/game.page.ts
+++ src/app/game/game.page.ts
@@ -62,12 +62,12 @@ export class GamePage implements OnInit {
   northPsg() {
     switch (this.currRoom) {
       case 'Room A':
-        console.log('You cannot go here');
+        this.noPsg();
         break;
       case 'Room B':
         if (!this.trollA)  {
-          console.log('A troll blocks the other side');
           this.playerScore = this.playerScore - 10;
+          this.roomBlocked();
         } else {
           this.currRoom = 'Room A';
           this.roomB = false;
@@ -85,7 +85,6 @@ export class GamePage implements OnInit {
         this.newRoom();
     }
     this.roomNav();
-    console.log(this.currRoom);
   }
 
   southPsg() {
@@ -97,11 +96,11 @@ export class GamePage implements OnInit {
         break;
       case 'Room B':
         if (!this.trollB) {
-          console.log(' a troll blocks this passage');
           this.playerScore = this.playerScore - 10;
+          this.trollHere();
         } else if (!this.trollC) {
-          console.log('A troll is in the other room');
           this.playerScore = this.playerScore - 10;
+          this.roomBlocked();
         } else {
           this.currRoom = 'Room C';
           this.roomB = false;
@@ -109,7 +108,7 @@ export class GamePage implements OnInit {
         }
         break;
       case 'Room C':
-        console.log('You cannot go here');
+        this.noPsg();
         break;
       case 'Room D':
         this.currRoom = 'Room C';
@@ -123,7 +122,7 @@ export class GamePage implements OnInit {
     switch (this.currRoom) {
       case 'Room A':
         if (!this.trollA) {
-          console.log('A troll is blocking this passage');
+          this.trollHere();
           this.playerScore = this.playerScore - 10;
         } else {
           this.currRoom = 'Room B';
@@ -132,11 +131,11 @@ export class GamePage implements OnInit {
         }
         break;
       case 'Room B':
-        console.log('You cannot go this way');
+        this.noPsg();
         break;
       case 'Room C':
         if (!this.trollC) {
-          console.log('A troll is blocking this passage');
+          this.trollHere();
           this.playerScore = this.playerScore - 10;
         } else {
           this.currRoom = 'Room B';
@@ -150,13 +149,12 @@ export class GamePage implements OnInit {
         this.newRoom();
     }
     this.roomNav();
-    console.log(this.currRoom);
   }
 
   westPsg() {
     switch (this.currRoom) {
       case 'Room A':
-        console.log('You cannot go here');
+        this.noPsg();
         break;
       case 'Room B':
         this.currRoom = 'Room D';
@@ -164,18 +162,17 @@ export class GamePage implements OnInit {
         this.newRoom();
         break;
       case 'Room C':
-        console.log('You cannot go here');
+        this.noPsg();
         break;
       case 'Room D':
         if (!this.enemiesLeft) {
           this.isExit = true;
           console.log('Exit Reached!');
         } else {
-          console.log('You cannot go here');
+          this.noPsg();
         }
     }
     this.roomNav();
-    console.log(this.currRoom);
   }
 
   // collect items
@@ -183,70 +180,74 @@ export class GamePage implements OnInit {
   collectCoin(){
     switch (this.currRoom) {
       case 'Room A':
-        console.log('There are no coins');
+        this.noCoin();
         break;
       case 'Room B':
         if (this.coinB === 0){
-          console.log('There are no coins');
+          this.noCoin();
         } else {
           this.playerScore = this.playerScore + this.coinB;
+          this.addCoin();
           this.coinB = 0;
         }
         break;
       case 'Room C':
         if (this.coinC === 0){
-          console.log('There are no coins');
+          this.noCoin();
         } else {
           this.playerScore = this.playerScore + this.coinC;
+          this.addCoin();
           this.coinC = 0;
         }
         break;
       case 'Room D':
         if (this.coinD === 0){
-          console.log('There are no coins');
+          this.noCoin();
         } else {
           this.playerScore = this.playerScore + this.coinD;
+          this.addCoin();
           this.coinD = 0;
         }
     }
-    console.log(this.playerScore);
   }
 
-  collectBomb(){
+  collectArrow(){
     switch (this.currRoom) {
       case 'Room A':
         if (this.arrowA === 0){
-          console.log('All bombs collected');
+          this.arrowsCollected();
         } else {
           this.playerArrows = this.playerArrows + this.arrowA;
+          this.addArrow();
           this.arrowA = 0;
         }
         break;
       case 'Room B':
         if (this.arrowB === 0){
-          console.log('There are no arrows');
+          this.arrowsCollected();
         } else {
           this.playerArrows = this.playerArrows + 1;
+          this.addArrow();
           this.arrowB = this.arrowB - 1;
         }
         break;
       case 'Room C':
-        console.log('There are no bombs here');
+        this.arrowsCollected();
         break;
       case 'Room D':
-        console.log('There are no bombs here');
+        this.arrowsCollected();
     }
   }
 // defeat enemy
 
   attack() {
     if (this.playerArrows < 1 ){
-      console.log('You have no bombs');
+      this.noArrows();
     } else {
       switch (this.currRoom) {
         case 'Room A':
           if (this.trollA){
-            console.log('No trolls in this room');
+            this.noTrolls();
           } else {
             this.playerArrows = this.playerArrows - 1;
             this.enemiesLeft = this.enemiesLeft - 1;
@@ -255,7 +256,7 @@ export class GamePage implements OnInit {
           break;
         case 'Room B':
           if (this.trollB){
-            console.log('There are no trolls in this room');
+            this.noTrolls();
           } else {
             this.playerArrows = this.playerArrows - 1;
             this.enemiesLeft = this.enemiesLeft - 1;
@@ -264,7 +265,7 @@ export class GamePage implements OnInit {
           break;
         case 'Room C':
           if (this.trollC){
-            console.log('There are no trolls in this room');
+            this.noTrolls();
           } else {
             this.playerArrows = this.playerArrows - 1;
             this.enemiesLeft = this.enemiesLeft - 1;
@@ -272,11 +273,14 @@ export class GamePage implements OnInit {
           }
           break;
           case 'Room D':
-          console.log('There are no trolls');
+          this.noTrolls();
       }
     }
 
   }
+
+
+  // Player notifications
 
   async newRoom() {
     const toast = await this.toastController.create({
@@ -286,6 +290,88 @@ export class GamePage implements OnInit {
     });
     toast.present();
   }
+
+  async trollHere() {
+    const toast = await this.toastController.create({
+      message: `A troll is blocking this passage! -10 points.`,
+      position: 'top',
+      duration: 1000
+    });
+    toast.present();
+  }
+
+  async roomBlocked() {
+    const toast = await this.toastController.create({
+      message: `A troll is blocking the next room! Go another way. -10 points.`,
+      position: 'top',
+      duration: 2500
+    });
+    toast.present();
+  }
+
+  async noPsg() {
+    const toast = await this.toastController.create({
+      message: `You cannot go that way`,
+      position: 'top',
+      duration: 1000
+    });
+    toast.present();
+  }
+
+  async addCoin() {
+    const toast = await this.toastController.create({
+      message: `Coin Collected`,
+      position: 'top',
+      duration: 1000
+    });
+    toast.present();
+  }
+
+  async noCoin() {
+    const toast = await this.toastController.create({
+      message: `There are no coins to collect`,
+      position: 'top',
+      duration: 1000
+    });
+    toast.present();
+  }
+
+  async addArrow() {
+    const toast = await this.toastController.create({
+      message: `Arrow Collected!`,
+      position: 'top',
+      duration: 1000
+    });
+    toast.present();
+  }
+
+  async arrowsCollected() {
+    const toast = await this.toastController.create({
+      message: `There are no arrows to collect`,
+      position: 'top',
+      duration: 1000
+    });
+    toast.present();
+  }
+
+  async noArrows() {
+    const toast = await this.toastController.create({
+      message: `You Have no arrows!`,
+      position: 'top',
+      duration: 1000
+    });
+    toast.present();
+  }
+
+  async noTrolls() {
+    const toast = await this.toastController.create({
+      message: `You Have no arrows!`,
+      position: 'top',
+      duration: 1000
+    });
+    toast.present();
+  }
+
 
 }
 
